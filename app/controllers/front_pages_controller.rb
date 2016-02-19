9d807378e2180f68051f31080b21b094c5ff446f
Rafal <rafaljamroz@gmail.com>
2016-02-19 02:22:59 +0100

    adding etag on front page

--- app/controllers/front_pages_controller.rb
+++ app/controllers/front_pages_controller.rb
@@ -1,4 +1,7 @@
 class FrontPagesController < ApplicationController
   def index
+    # HEROKU_SLUG_COMMIT env var requires enabled heroku runtime-dyno-metadata
+    etag = ENV['HEROKU_SLUG_COMMIT'] || 'fake_etag'
+    fresh_when(etag: etag, public: true)
   end
 end
